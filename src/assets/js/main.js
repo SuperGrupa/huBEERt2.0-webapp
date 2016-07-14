d780d31611f3eb6655667fdf24694552d83a69a6
Nitrooos <bartoszkostaniak@gmail.com>
2016-07-14 17:10:53 +0200

    Poprawiona wersja na Ff, lepsza responsywność

--- src/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  var header = $('header'),
-      footer = $('footer'),
-      content = $('.offset-header');
-
-  $(content).css("paddingTop", $(header).height());
-  $(content).height($(window).height() - $(footer).height() - $(header).height());
-});
