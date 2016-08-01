2be206104a60df44d1f6b30c73464de81a3fc340
Nitrooos <bartoszkostaniak@gmail.com>
2016-08-01 22:18:36 +0200

    Dodanie modelu City, serwisu CityService i uwzględnianie miasta w wyszukiwarce

---
+++ src/app/search/service/city.service.ts
@@ -0,0 +1,28 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import { City } from '../model/city';
+import Url from 'urls';
+
+@Injectable()
+export class CityService {
+  constructor (private http: Http) { }
+
+  getCities(): Observable<City[]> {
+    return this.http.get(Url.cities())
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body || { };
+  }
+
+  private handleError (error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
