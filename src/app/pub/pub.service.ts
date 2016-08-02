3540dc6a4fb073632fbda13ca311c5acd5693c64
Nitrooos <bartoszkostaniak@gmail.com>
2016-08-02 18:18:40 +0200

    Wyświetlanie komentarzy na stronie pubu

--- src/app/pub/pub.service.ts
+++ src/app/pub/pub.service.ts
@@ -1,6 +1,7 @@
 import { Injectable }     from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Pub }            from './model/pub';
+import { Comment }        from './model/comment';
 import { Observable }     from 'rxjs/Observable';
 import Url from 'urls';
 
--- src/app/pub/show/comments/comments.component.ts
+++ src/app/pub/show/comments/comments.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 
 import { PubService } from '../../pub.service';
+import { Comment } from '../../model/comment';
+import { Pagination } from '../../../common/pagination/pagination.component';
 
 @Component({
   selector: 'pub-show-comments',
   template: require('./comments.component.html'),
   styles: [require('./comments.component.scss')],
   providers: [PubService],
+  directives: [Pagination],
 })
 
 export class PubShowCommentsComponent implements OnInit {
   @Input() pub_id: number;
-  comments: Comment[];
+  comments: Comment[] = [];
+  active_comments: Comment[] = [];
   error_message: string;
+  current_page: number = 1;
+
+  PAGE_SIZE = 10;
 
   constructor(private pubService: PubService) { }
 
@@ -20,9 +27,21 @@ export class PubShowCommentsComponent implements OnInit {
     this.getPubComments();
   }
 
+  pageChanged(page) {
+    this.current_page = page;
+    let begin = (this.current_page - 1)*this.PAGE_SIZE;
+    this.active_comments = this.comments.slice(begin, begin + this.PAGE_SIZE);
+  }
+
   getPubComments() {
     this.pubService.getComments(this.pub_id).subscribe(
-      data => this.comments = data,
+      data => {
+        this.comments = data.sort((c1, c2) => {
+          if (c1.date > c2.date) return -1
+          else return 1;
+        });
+        this.pageChanged(1);
+      },
       error => this.error_message = <any>error
     );
   }
