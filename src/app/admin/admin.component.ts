2ceae643aab2d345af66593a85243308fc1ced9b
Nitrooos <bartoszkostaniak@gmail.com>
2016-08-16 14:32:02 +0200

    Początek prac nad panelem admina

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin',
+  template: require('./admin.component.html'),
+  styles: [require('./admin.component.scss')],
+})
+
+export class AdminComponent {
+
+}
